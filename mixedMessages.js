32bfd8d566d6c800f4553fe1355147bef4aad254
Sahith Gullapalli <sahith.gullapalli433@gmail.com>
2022-01-18 15:09:55 -0600

    Add comments to script

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,7 +1,10 @@
+// messages object
 let mixedMessages = {
+    // arrays to create a nonsensical sentence
     character: ['Sahith', 'Nikhil', 'Kashish', 'Shiva', 'Neo', 'Shay', 'Paviesh', 'Sheevam', 'Parth', 'Yuvan'],
     verbs: ['jumping at', 'a bug who gave a hug to', 'able to talk with', 'taking a walk to', 'playing pool with', 'at Halal Guys with'],
     connectors: ['a park', 'the bear', 'a computer', 'all the bees'],
+    // Methods to get a random element from each array
     getRandChar() {
         return this.character[Math.floor(Math.random() * this.character.length)];
     },
@@ -12,10 +15,14 @@ let mixedMessages = {
         return this.connectors[Math.floor(Math.random() * this.connectors.length)];
     }
 }
+// arrow function to display the nonsensical sentence
 const display = () => {
+    // calling object methods to return a random element
     let char = mixedMessages.getRandChar();
     let verb = mixedMessages.getRandVerb();
     let connec = mixedMessages.getRandConnector();
+    // return the sentence
     return `${char} was ${verb} ${connec}`;
 }
+// to log the random sentence
 console.log(display());
